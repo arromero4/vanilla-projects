e96bb377cc327ef31e19d3f30341c9c16a446757
arromero4 <arromero0408@gmail.com>
2024-02-11 01:20:19 -0600

    adding password generator app - check execCommand because is deprecated

---
+++ 08-Declaracion/script.js
@@ -0,0 +1,18 @@
+const yesBtn = document.querySelector('#yesBtn');
+const noBtn = document.querySelector('#noBtn');
+
+yesBtn.addEventListener('click', function() {
+    alert('Sabia que si querias!')
+})
+
+noBtn.addEventListener('mouseover', function() {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+
+    noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+
+
+});
